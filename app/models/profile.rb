3f8a93e7a87db91156e2a02ac489f81ba38aea76
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-03 15:05:23 +0900

    バリデーション

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,4 +5,35 @@ class Profile < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   
+  with_options presence: true do 
+    validates :name
+    validates :explanation
+    validates :image
+    with_option :numericality :true do
+      validates :insta_follower, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_man, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_woman, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_age1, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_age2, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_age3, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_age4, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_age5, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_age6, format: { with: /\A[0-9０-９]+\z/ }
+      validates :insta_age7, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_follower, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_man, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_woman, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_age1, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_age2, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_age3, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_age4, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_age5, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_age6, format: { with: /\A[0-9０-９]+\z/ }
+      validates :youtube_age7, format: { with: /\A[0-9０-９]+\z/ }
+      validates :viewing, format: { with: /\A[0-9０-９]+\z/ }
+    end
+    with_options  numericality: { other_than: 0 } do
+      validates :category_id
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+          validates :nickname
+          validates :email
+          validates :password, format: { with: /\A[a-z0-9]+\z/i }
+          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }
+         end 
+
 end
