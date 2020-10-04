6c645a940e2e13f317bb2caa4368db665b4d1139
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-04 19:59:10 +0900

    修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -24,7 +24,7 @@ class ProfilesController < ApplicationController
 private
 
   def create_params
-    params.require(:profile).permit(:image, :name, :explanation, :insta_follower, :insta_man, :insta_woman, :insta_age1, :insta_age2, :insta_age3, :insta_age4, :insta_age5, :insta_age6, :insta_age7, :youtube_follower, :youtube_man, :youtube_woman, :youtube_age1, :youtube_age2, :youtube_age3, :youtube_age4, :youtube_age5, :youtube_age6, :youtube_age7, :viewing, :category_id, :user_id).merge(user_id: current_user.id)
+    params.require(:profile).permit(:image, :name, :explanation, :insta_follower, :insta_man, :insta_woman, :insta_age1, :insta_age2, :insta_age3, :insta_age4, :insta_age5, :insta_age6, :insta_age7, :youtube_follower, :youtube_man, :youtube_woman, :youtube_age1, :youtube_age2, :youtube_age3, :youtube_age4, :youtube_age5, :youtube_age6, :youtube_age7, :viewing, :category_id).merge(user_id: current_user.id)
   end
 
 end
