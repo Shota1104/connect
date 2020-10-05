f201f02c272882ffcfe1bc5a94df0f819deb7b09
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-05 20:18:42 +0900

    編集と削除機能実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -31,9 +31,35 @@ class ProfilesController < ApplicationController
     gon.profile = @profile
   end
 
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update(update_params)
+       redirect_to profile_path
+    else
+       render :edit
+    end
+  end
+
+  def destroy
+    @profile = Profile.find(params[:id])
+    if @profile.delete
+       redirect_to root_path
+    else
+      render :show
+    end
+  end
+
 private
 
   def create_params
+    params.require(:profile).permit(:image, :name, :explanation, :insta_follower, :insta_man, :insta_woman, :insta_age1, :insta_age2, :insta_age3, :insta_age4, :insta_age5, :insta_age6, :insta_age7, :youtube_follower, :youtube_man, :youtube_woman, :youtube_age1, :youtube_age2, :youtube_age3, :youtube_age4, :youtube_age5, :youtube_age6, :youtube_age7, :viewing, :category_id).merge(user_id: current_user.id)
+  end
+
+  def update_params
     params.require(:profile).permit(:image, :name, :explanation, :insta_follower, :insta_man, :insta_woman, :insta_age1, :insta_age2, :insta_age3, :insta_age4, :insta_age5, :insta_age6, :insta_age7, :youtube_follower, :youtube_man, :youtube_woman, :youtube_age1, :youtube_age2, :youtube_age3, :youtube_age4, :youtube_age5, :youtube_age6, :youtube_age7, :viewing, :category_id).merge(user_id: current_user.id)
   end
 
