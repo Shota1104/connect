50519b50d082fafbfa93df1083fd0cc613bbea58
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-06 11:32:20 +0900

    カラム追加＆追加によるviewの修正

--- db/migrate/20201001033832_create_profiles.rb
+++ db/migrate/20201001033832_create_profiles.rb
@@ -13,17 +13,20 @@ class CreateProfiles < ActiveRecord::Migration[6.0]
       t.integer :insta_age5,         null:false
       t.integer :insta_age6,         null:false
       t.integer :insta_age7,         null:false
-      t.integer :youtube_follower,   null:false
-      t.integer :youtube_man,        null:false
-      t.integer :youtube_woman,      null:false
-      t.integer :youtube_age1,       null:false
-      t.integer :youtube_age2,       null:false
-      t.integer :youtube_age3,       null:false
-      t.integer :youtube_age4,       null:false
-      t.integer :youtube_age5,       null:false
-      t.integer :youtube_age6,       null:false
-      t.integer :youtube_age7,       null:false
-      t.integer :viewing,            null:false
+      t.integer :insta_reach,        null:false
+      t.integer :insta_impression,   null:false
+      t.integer :insta_click,        null:false
+      t.integer :youtube_follower
+      t.integer :youtube_man
+      t.integer :youtube_woman
+      t.integer :youtube_age1
+      t.integer :youtube_age2
+      t.integer :youtube_age3
+      t.integer :youtube_age4
+      t.integer :youtube_age5
+      t.integer :youtube_age6
+      t.integer :youtube_age7
+      t.integer :viewing
       t.integer :category_id,        null:false
       t.references :user,            null: false, foreign_key: true
       t.timestamps
