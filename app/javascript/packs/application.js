221d3a7725ad507284545bf73955a4e6819bfd0e
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-15 16:14:51 +0900

    heroku修正２

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require('../profile_show')
-require("chartkick")
 require("chart.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
