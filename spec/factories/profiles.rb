4008d4ac42c660f534d91bf59b13e16de80a40ba
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-18 17:58:19 +0900

    ファクトリーボット

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :profile do
-    
+    name             {"チャンネルch漢字"}
+    explanation      {"これはテストです。単価10万"}
+    insta_follower   {"20000"}
+    insta_man        {"50"}
+    insta_woman      {"50"}
+    insta_age1       {"10"}
+    insta_age2       {"10"}
+    insta_age3       {"10"}
+    insta_age4       {"10"}
+    insta_age5       {"20"}
+    insta_age6       {"20"}
+    insta_age7       {"20"}
+    insta_reach      {"9000"}
+    insta_impression {"400"}
+    insta_click      {"20"}
+    youtube_follower {"10000"}
+    twitter_follower {"10000"}
   end
 end
