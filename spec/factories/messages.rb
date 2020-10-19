d866b42cb6cc40b451fccd874c926ddf381cfb3e
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-19 14:34:16 +0900

    メッセージ単体テストコード

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :message do
-    
+    content   {"こんにちは"}
+    association :user
+    association :room
+    after(:build) do |profile|
+      profile.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @message = FactoryBot.build(:message)
+    end
+    context '登録がうまくいくとき' do
+      it 'contentとimageが存在していれば保存できること' do
+        expect(@message).to be_valid
+      end
+      it 'contentが空でも保存できること' do
+        @message.content = nil
+        expect(@message).to be_valid
+      end
+      it 'imageが空でも保存できること' do
+        @message.image = nil
+        expect(@message).to be_valid
+      end
+    end
+
+    context '登録がうまくいかないとき' do
+      it 'contentとimageが空では保存できないこと' do
+        @message.content = nil
+        @message.image = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Contentを入力してください")
+      end
+      it 'roomが紐付いていないと保存できないこと' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Roomを入力してください")
+      end
+      it 'userが紐付いていないと保存できないこと' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Userを入力してください")
+      end
+    end
+  end
 end
