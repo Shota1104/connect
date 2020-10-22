44265592320f40f896707c0a586d60b552dcbb22
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-10-22 18:00:01 +0900

    active strogeバリデーション

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -37,7 +37,6 @@ class Profile < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :image
     with_options numericality: true do
       validates :insta_follower, format: { with: /\A[0-9０-９]{5,}\z/ }
       validates :insta_man, format: { with: /\A[0-9０-９]+\z/ }
@@ -52,6 +51,18 @@ class Profile < ApplicationRecord
     end
     with_options numericality: { other_than: 0 } do
       validates :category_id
+    end
+  end
+  
+  validate :image_presence
+
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
     end
   end
 end
